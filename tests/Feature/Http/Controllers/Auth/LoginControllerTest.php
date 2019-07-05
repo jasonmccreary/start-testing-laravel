1d2047780f703b4491b59bccd361c3461681bd45
Jason McCreary <jason@pureconcepts.net>
2019-07-05 09:22:09 -0400

    Test validation fails

--- tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
@@ -14,4 +14,13 @@ class LoginControllerTest extends TestCase
         $response->assertStatus(200);
         $response->assertViewIs('auth.login');
     }
+
+    /** @test */
+    public function login_displays_validation_errors()
+    {
+        $response = $this->post('/login', []);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+    }
 }
