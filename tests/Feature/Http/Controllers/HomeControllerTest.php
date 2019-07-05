39b96a884147ffa26985b7598fb5df6e7f8aa670
Jason McCreary <jason@pureconcepts.net>
2019-07-05 15:36:29 -0400

    Test HomeController

---
+++ tests/Feature/Http/Controllers/HomeControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class HomeControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function index_returns_a_view()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('home'));
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function index_redirects_when_unauthorized()
+    {
+        $response = $this->get(route('home'));
+
+        $response->assertStatus(302);
+    }
+}
